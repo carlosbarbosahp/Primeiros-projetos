a75ee66e104f41cfeb8a3c0003167b654af6ab8c
Carlos Henrique <carlos.hpbarbosa@hotmail.com>
2022-04-05 19:57:56 -0300

    Alteração nome das pastas

--- projeto10 - Matriz/projeto10/Program.cs
+++ projeto10 - Matriz/projeto10/Program.cs
@@ -22,7 +22,7 @@ namespace Course
                 }
             }
 
-            int x = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine);
 
             for (int i = 0; i < m; i++)
             {
